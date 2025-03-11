4e0f221f21f82c1a92a56834c4865f22bcf5bbe5
Arnav <arnav.das88@gmail.com>
2025-03-11 11:38:18 +0000

    re-changed the file format and added package_data

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import Extension, setup
+from setuptools import Extension, setup, find_packages
 from setuptools.command.sdist import sdist as SetuptoolsSdist
 
 # This version string should be updated when releasing a new version.
@@ -6,11 +6,15 @@ _VERSION = '0.0.1'
 
 setup(
     name = 'py-measuredns',
-    packages=["measure_dns"],
-    version = '1.0',
+    version="0.1",
+    packages=find_packages(),
     description = 'A python library to measure dns query latency, accurately',
     author = 'Arnav Das',
     include_package_data=True,
+    ## Include data files
+    package_data={
+        "measure_dns": ["measuredns.c"],
+    },
     requires=["wheel", "dnspython"],
     author_email = 'arnav.das88@gmail.com',
     url = 'https://docs.python.org/extending/building',
