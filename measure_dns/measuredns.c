e740155868249786b9d400b0987feb4212db2ec2
Arnav <arnav.das88@gmail.com>
2025-03-11 09:59:35 +0000

    Cleaned measuredns.c

--- measure_dns/measuredns.c
+++ measure_dns/measuredns.c
@@ -1,5 +1,9 @@
 /*
+ * Compile with:
  * $ gcc -shared -o measuredns.so -fPIC measuredns.c 
+ *
+ * This code sends a DNS query with optional PDM metrics and processes the response.
+ * It supports both IPv4 and IPv6.
  */
 
 #include <stdio.h>
@@ -13,52 +17,55 @@
 #include <sys/time.h>
 #include <time.h>
 
-#define MAX_DNS_PACKET_SIZE 512
-#define PDM_EXTHDR_SIZE 16
 
- // DNS Flag Definitions
-#define DNS_FLAG_NO_FLAG 0x0000
-#define DNS_FLAG_PDM_METRIC 0x0001
-#define DNS_FLAG_PRE_RESOLVE4 0x0010
-#define DNS_FLAG_PRE_RESOLVE6 0x0100
- 
+#define MAX_DNS_PACKET_SIZE 512  // Max size of a DNS packet
+#define PDM_EXTHDR_SIZE 16       // PDM Extension Header size
+
+// DNS Flag Definitions
+#define DNS_FLAG_NO_FLAG       0x0000  // No special flag
+#define DNS_FLAG_PDM_METRIC    0x0001  // Enable PDM metric extension
+#define DNS_FLAG_PRE_RESOLVE4  0x0010  // Pre-resolve IPv4 address
+#define DNS_FLAG_PRE_RESOLVE6  0x0100  // Pre-resolve IPv6 address
+
+// Structure for IPv6 Destination Options Header (Including PDM)
 struct dest_opt_hdr {
-    uint8_t next_header;   // Next header after this extension
-    uint8_t hdr_ext_len;   // Header extension length (in 8-octet units)
-    uint8_t options[14];   // PDM option + padding (14 bytes)
+    uint8_t next_header;  // Next header after this extension (e.g., UDP)
+    uint8_t hdr_ext_len;  // Extension header length in 8-octet units (excluding first 8 bytes)
+    uint8_t options[14];  // PDM option + required padding (14 bytes)
 };
- 
+
+// Structure for Performance and Diagnostic Metrics (PDM) option
 struct pdm_option {
-    uint8_t  option_type;  // 0x0F (00001111)
-    uint8_t  opt_len;      // 10 (length excluding type and length fields)
-    uint8_t  scale_dtlr;   // Scale for Delta Time Last Received
-    uint8_t  scale_dtls;   // Scale for Delta Time Last Sent
-    uint16_t psntp;        // Packet Sequence Number This Packet
-    uint16_t psnlr;        // Packet Sequence Number Last Received
-    uint16_t deltatlr;     // Delta Time Last Received
-    uint16_t deltatls;     // Delta Time Last Sent
-} __attribute__((aligned(4)));
+    uint8_t  option_type;   // Option type (fixed: 0x0F)
+    uint8_t  opt_len;       // Length excluding type & length fields (fixed: 10)
+    uint8_t  scale_dtlr;    // Scale factor for Delta Time Last Received
+    uint8_t  scale_dtls;    // Scale factor for Delta Time Last Sent
+    uint16_t psntp;         // Packet Sequence Number This Packet
+    uint16_t psnlr;         // Packet Sequence Number Last Received
+    uint16_t deltatlr;      // Delta Time Last Received
+    uint16_t deltatls;      // Delta Time Last Sent
+} __attribute__((aligned(4)));  // Ensure 4-byte alignment
 
-// Additional parameters storage
+// Maximum additional parameters for DNS response
 #define MAX_ADDITIONAL_PARAMS 5
-#define MAX_PARAM_SIZE 32
+#define MAX_PARAM_SIZE 32  // Size of additional parameter buffer
 
+// Structure for additional parameters
 typedef struct {
-    int type;  // Identifier for the parameter type
-    unsigned char data[MAX_PARAM_SIZE]; // Buffer to store the parameter
+    int type;  // Parameter type identifier
+    unsigned char data[MAX_PARAM_SIZE]; // Parameter data buffer
 } AdditionalParam;
 
-// Structure to hold query response details
+// Structure to hold DNS response details
 typedef struct {
-    int response_size;
-    double latency_ns;
-    unsigned char response[MAX_DNS_PACKET_SIZE];
+    int response_size;  // Response size in bytes
+    double latency_ns;  // Query latency in nanoseconds
+    unsigned char response[MAX_DNS_PACKET_SIZE];  // Buffer for DNS response
 
-    int num_additional_params;
-    // AdditionalParam additional_params[MAX_ADDITIONAL_PARAMS];
-    AdditionalParam *additional_params;
+    int num_additional_params;  // Number of additional parameters
+    AdditionalParam *additional_params;  // Pointer to additional parameters array
 } DNSResponse;
-
+// Generate a random Packet Sequence Number (PSN) using /dev/urandom
 uint16_t get_random_psn() {
     uint16_t psn;
     int fd = open("/dev/urandom", O_RDONLY);
@@ -70,11 +77,16 @@ uint16_t get_random_psn() {
     return psn;
 }
 
+// Get the current time in nanoseconds
 static inline void time_get_real_ns(struct timespec *ts) {
     clock_gettime(CLOCK_MONOTONIC_RAW, ts);
 }
 
-// Function to send a raw DNS request and receive a response
+/*
+ * Sends a raw DNS query and receives the response.
+ * Supports both IPv4 and IPv6. If PDM metrics are enabled, they are embedded
+ * in the IPv6 Destination Options Header.
+ */
 int dns_query(const char* dns_server, unsigned char* request, int req_size, DNSResponse* result, int use_ipv6, int flags) {
     int sockfd;
     struct sockaddr_storage dest;
@@ -91,43 +103,37 @@ int dns_query(const char* dns_server, unsigned char* request, int req_size, DNSR
 
         sockfd = socket(AF_INET6, SOCK_DGRAM, IPPROTO_UDP);
 
+        // If PDM metric flag is set, configure PDM in IPv6 Destination Options
         if (flags & DNS_FLAG_PDM_METRIC) {
-
-            // Create and populate the PDM option
-            struct pdm_option pdm;
-            pdm.option_type = 0x0F;   // PDM Option Type
-            pdm.opt_len = 10;         // Length (excluding type and len fields)
-            pdm.scale_dtlr = 0;       // Example scale for Delta Time Last Received
-            pdm.scale_dtls = 0;       // Example scale for Delta Time Last Sent
-            pdm.psntp = htons(get_random_psn());  // Example Packet Sequence Number This Packet
-            pdm.psnlr = htons(0);     // Example Packet Sequence Number Last Received
-            pdm.deltatlr = htons(0);  // Example Delta Time Last Received
-            pdm.deltatls = htons(0);  // Example Delta Time Last Sent
+            struct pdm_option pdm = {
+                .option_type = 0x0F,   // PDM Option Type
+                .opt_len = 10,         // Length (excluding type and len fields)
+                .scale_dtlr = 0,       // Example scale for Delta Time Last Received
+                .scale_dtls = 0,       // Example scale for Delta Time Last Sent
+                .psntp = htons(get_random_psn()),  // Example Packet Sequence Number This Packet
+                .psnlr = htons(0),     // Example Packet Sequence Number Last Received
+                .deltatlr = htons(0),  // Example Delta Time Last Received
+                .deltatls = htons(0)  // Example Delta Time Last Sent
+            };
 
             // Create the Destination Options Header and embed PDM
-            struct dest_opt_hdr dstopt;
-            memset(&dstopt, 0, sizeof(dstopt));
+            struct dest_opt_hdr dstopt = {0};
+            dstopt.next_header = 17;  // IPPROTO_UDP  (Next header is UDP)
+            dstopt.hdr_ext_len = 1;   // Length in 8-octet units, excluding first 8 octets
 
-            dstopt.next_header = 17; // IPPROTO_UDP (Next header is UDP)
-            dstopt.hdr_ext_len = 1;  // Length in 8-octet units, excluding first 8 octets
-
-            // Copy the PDM option into the options field
+            // Copy PDM into options field and add padding
             memcpy(dstopt.options, &pdm, sizeof(pdm));
+            memset(dstopt.options + sizeof(pdm), 0x00, 14 - sizeof(pdm));
 
-            // Padding to make the total length 14 bytes (as required)
-            memset(dstopt.options + sizeof(pdm), 0x00, 14 - sizeof(pdm)); // This defines padding
-            // memset(dstopt.options + sizeof(pdm), 0x01, 1); // This defines padding type
-
-            int status = setsockopt(sockfd, IPPROTO_IPV6, IPV6_DSTOPTS, &dstopt, sizeof(dstopt));
-            if (status < 0) {
+            // Set Destination Options Header in socket
+            if (setsockopt(sockfd, IPPROTO_IPV6, IPV6_DSTOPTS, &dstopt, sizeof(dstopt)) < 0) {
                 perror("setsockopt DSTOPT");
             }
 
-            // Allow the kernel to pass destination options to the application
+            // Allow kernel to pass destination options to application
             if (setsockopt(sockfd, IPPROTO_IPV6, IPV6_RECVDSTOPTS, &opt, sizeof(opt)) < 0) {
                 perror("setsockopt IPV6_RECVDSTOPTS");
                 close(sockfd);
-                exit(EXIT_FAILURE);
             }
         }
 
@@ -153,18 +159,22 @@ int dns_query(const char* dns_server, unsigned char* request, int req_size, DNSR
     } else {
         struct sockaddr_in* dest4 = (struct sockaddr_in*)&dest;
         sockfd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+
         if (sockfd < 0) {
             perror("Socket creation failed (IPv4)");
             return -1;
         }
+
         dest4->sin_family = AF_INET;
         dest4->sin_port = htons(53);
         dest4->sin_addr.s_addr = inet_addr(dns_server);
         dest_len = sizeof(struct sockaddr_in);
     }
 
+    // Measure start time
     time_get_real_ns(&start);
 
+    // Send the DNS query
     if (sendto(sockfd, request, req_size, 0, (struct sockaddr*)&dest, dest_len) < 0) {
         perror("Query sending failed");
         close(sockfd);
@@ -172,22 +182,15 @@ int dns_query(const char* dns_server, unsigned char* request, int req_size, DNSR
     }
 
 
-    // Prepare to receive the message along with ancillary data
+    // Prepare to receive response
     char ctrl_buf[1024];
-    struct iovec iov;
-    struct msghdr msg;
-    memset(&msg, 0, sizeof(msg));
-
-    iov.iov_base = result->response;
-    iov.iov_len = MAX_DNS_PACKET_SIZE;
-    msg.msg_iov = &iov;
-    msg.msg_iovlen = 1;
-
-    msg.msg_control = ctrl_buf;
-    msg.msg_controllen = sizeof(ctrl_buf);
+    struct iovec iov = { .iov_base = result->response, .iov_len = MAX_DNS_PACKET_SIZE };
+    struct msghdr msg = { 
+        .msg_iov = &iov, .msg_iovlen = 1, 
+        .msg_control = ctrl_buf, .msg_controllen = sizeof(ctrl_buf) 
+    };
 
     ssize_t resp_size = recvmsg(sockfd, &msg, 0);
-
     time_get_real_ns(&end);
 
     if (resp_size < 0) {
@@ -200,14 +203,11 @@ int dns_query(const char* dns_server, unsigned char* request, int req_size, DNSR
     struct cmsghdr *cmsg;
     for (cmsg = CMSG_FIRSTHDR(&msg); cmsg != NULL; cmsg = CMSG_NXTHDR(&msg, cmsg)) {
         if (cmsg->cmsg_level == IPPROTO_IPV6 && cmsg->cmsg_type == IPV6_DSTOPTS) {
-            // printf("Received IPv6 Destination Options header!\n");
             // cmsg->cmsg_data points to the received destination options header.
             // You can cast it to a structure pointer (if defined) to process it.
             // For example:
             // struct ipv6_destopt *dstopts = (struct ipv6_destopt *)CMSG_DATA(cmsg);
             if (result->num_additional_params < MAX_ADDITIONAL_PARAMS) {
-                // result->additional_params[param_index].type = IPV6_DSTOPTS;
-                // memcpy(result->additional_params[param_index].data, CMSG_DATA(cmsg), MAX_PARAM_SIZE);
                 result->additional_params[result->num_additional_params].type = IPV6_DSTOPTS;
                 memcpy(result->additional_params[result->num_additional_params].data, CMSG_DATA(cmsg), MAX_PARAM_SIZE);
                 result->num_additional_params++;
@@ -222,14 +222,10 @@ int dns_query(const char* dns_server, unsigned char* request, int req_size, DNSR
     return resp_size;
 }
 
+// Wrapper function for dns_query
 int query_dns(
-    const char* dns_server, 
-    unsigned char* request, 
-    int req_size, 
-    DNSResponse* result, 
-    int use_ipv6, 
-    int flags
+    const char* dns_server, unsigned char* request, int req_size, 
+    DNSResponse* result, int use_ipv6, int flags
 ) {
     return dns_query(dns_server, request, req_size, result, use_ipv6, flags);
 }
-  
